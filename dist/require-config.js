ef2fca90fd2ef844d295274713fbc8c2e6aa08fb
Your Name <you@example.com>
2022-11-18 18:01:24 +0100

    fix require knockout

--- dist/require-config.js
+++ dist/require-config.js
@@ -1,6 +1,6 @@
 require.config({
     paths: {
-        'knockout': '../vendor/knockout/knockout-3.5.1.debug.js',
+        'knockout': '../vendor/knockout/knockout-3.5.1.debug',
         'axios': '../vendor/axios/axios.min',
         'css': '../vendor/require-plugins/css.min'
     }
--- src/require-config.ts
+++ src/require-config.ts
@@ -2,7 +2,7 @@
 declare var require: any;
 require.config({
     paths: {
-        'knockout': '../vendor/knockout/knockout-3.5.1.debug.js',
+        'knockout': '../vendor/knockout/knockout-3.5.1.debug',
         'axios': '../vendor/axios/axios.min',
         'css': '../vendor/require-plugins/css.min'
     }
